1701636031a9973b8b6bc2d21b7468c53b72a261
yonggyu.song <your email>
2020-02-16 14:21:02 +0900

    first commit

---
+++ Cha Chat/AuthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthViewController.swift
+//  Cha Chat
+//
+//  Created by 송용규 on 13/02/2020.
+//  Copyright © 2020 송용규. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cha Chat/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  Cha Chat
+//
+//  Created by 송용규 on 16/02/2020.
+//  Copyright © 2020 송용규. All rights reserved.
+//
+
+import Foundation
